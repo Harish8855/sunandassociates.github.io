15e7e3a15c9f8fe6100f8c6b8d3bf0bde6433b32
Sohan Nehete <sunnclient.com>
2025-01-02 14:26:42 +0530

    Upadte

--- script.js
+++ script.js
@@ -2,3 +2,31 @@ function toggleNavbar() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
 }
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+// Change slide with buttons
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+// Display slides
+function showSlides(n) {
+    let slides = document.querySelectorAll(".slide");
+
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => slide.style.display = "none");
+    slides[slideIndex].style.display = "block";
+}
+
+// Auto slide every 5 seconds
+setInterval(() => {
+    changeSlide(1);
+}, 5000);
