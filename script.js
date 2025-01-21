676a8d92e9108923cccddcf2e381c2aa27442ece
Sohan Nehete <sunnclient.com>
2025-01-21 14:40:33 +0530

    done

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 // Hamburger menu toggle
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("nav-links");
+const hamburger = document.getElementById("mine-hamburger");
+const navLinks = document.getElementById("mine-nav-links");
 
 hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
-
-// if (navLinks.classList.contains("active")) {
-//     slideshow-container.classList.add("menu-open");
-// }
-// else{
-//     slideshow.classList.remove("menu-open");
-// }
+    navLinks.classList.toggle("mine-active");});
 
 // Slideshow functionality
 let slideIndex = 0;
-const slides = document.getElementsByClassName("slides");
+const slides = document.getElementsByClassName("mine-slides");
 
 function showSlide(index) {
     for (let i = 0; i < slides.length; i++) {
@@ -33,7 +25,5 @@ function prevSlide() {
     slideIndex = (slideIndex - 1 + slides.length) % slides.length;
     showSlide(slideIndex);
 }
-
-// Initialize slideshow
 showSlide(slideIndex);
 interwal = setInterval(nextSlide, 5000);
